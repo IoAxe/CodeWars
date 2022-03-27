0742509d967ab9bda66437cd75f81e15312547b5
igor <ivoivoivo@mail.ru>
2022-03-27 18:37:14 +0300

    If you can't sleep, just count sheepgit add index.js

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 
 "use strict";
 
+let countSheep = function (num) {
+    //your code here
+    let str = "sheep...";
+    let number = num;
+    let answer = "";
+    if (number > 0) {
+        for (let i = 0; i != number; i++) {
+            answer += (i + 1) + " " + str;
+        }
+        return answer;
+    } else {
+        return "";
+    }
+};
 
-
-
+console.log(countSheep(0));
 
 
 
